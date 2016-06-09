a8f55567852753c976d9ad79856b903cf1aa0581
Brianne Hostutler <briannehostutler@gmail.com>
2016-06-09 18:16:31 -0400

    changes

--- public/script.js
+++ public/script.js
@@ -1,49 +1,6 @@
 //get the previously scraped data from /articles
 $.getJSON('/articles', function(data) {
-  for (var i = 0; i<data.length; i++){
+  for (var i = 0; i<10; i++){
     $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
   }
 });
-
-//
-// $(document).on('click', 'p', function(){
-//   $('#notes').empty();
-//   var id = $(this).attr('data-id');
-//   $.ajax({
-//     method: "GET",
-//     url: "/articles/" + id,
-//   })
-//     .done(function( data ) {
-//       console.log(data);
-//       $('#notes').append('<h2>' + data.title + '</h2>');
-//       $('#notes').append('<input id="titleinput" name="title" >');
-//       $('#notes').append('<textarea id="bodyinput" name="body"></textarea>');
-//       $('#notes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
-//
-//       if(data.note){
-//         $('#titleinput').val(data.note.title);
-//         $('#bodyinput').val(data.note.body);
-//       }
-//     });
-// });
-//
-// $(document).on('click', '#save', function(){
-//   var thisId = $(this).attr('data-id');
-//
-//   $.ajax({
-//     method: "POST",
-//     url: "/articles/" + thisId,
-//     data: {
-//       title: $('#titleinput').val(),
-//       body: $('#bodyinput').val()
-//     }
-//   })
-//     .done(function( data ) {
-//       console.log(data);
-//       $('#notes').empty();
-//     });
-//
-//
-//   $('#titleinput').val("");
-//   $('#bodyinput').val("");
-// });
